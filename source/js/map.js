9c362cf5b0944d52944869e931bbb9582340a730
Анна Полуэктова <a.poluektova88@gmail.com>
2021-04-20 23:36:42 +0400

    Add form layout

---
+++ source/js/map.js
@@ -0,0 +1,20 @@
+ymaps.ready(init);
+
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [59.93863506417266,30.323117499999945],
+    zoom: 17,
+    controls: []
+  });
+
+  myMap.behaviors.disable("scrollZoom");
+
+  var myPlacemark = new ymaps.Placemark([59.93863506417266,30.323117499999945], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: 'img/ svg/map-pin.svg',
+    iconImageSize: [67, 100],
+    iconImageOffset: [-25, -100]
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+}
